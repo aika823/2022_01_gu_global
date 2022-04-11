d60155faf1a4e6fa6e28e82c0d208ab0971f5508
aika823 <aika823@naver.com>
2022-04-11 11:32:46 +0900

    로고 수정

--- gu_global/static/js/navigation.js
+++ gu_global/static/js/navigation.js
@@ -1,16 +1,19 @@
+console.log("navigation.js");
+
 $('.btn-menu').mouseover(function() {
-    $('.logo img').attr('src', '/static/images/logo_.png');
+    console.log("mouseover")
+    $('.logo img').attr('src', '/static/images/logo_2.png');
     $('header').addClass('white');
     $('.btn-menu').addClass('color');
     $('.btn-menu').removeClass('active');
     $(this).addClass('active');
     $('.header-content').css('display', 'block');
     if ($(this).hasClass('menu-company') == true) {
-      $('.category-box').css('display', 'none');
-      $('.company-category').css('display', 'block');
+        $('.category-box').css('display', 'none');
+        $('.company-category').css('display', 'block');
     } else if ($(this).hasClass('menu-products') == true) {
-      $('.category-box').css('display', 'none');
-      $('.product-category').css('display', 'grid');
+        $('.category-box').css('display', 'none');
+        $('.product-category').css('display', 'grid');
     } else if ($(this).hasClass('menu-solution') == true) {
         $('.category-box').css('display', 'none');
         $('.solution-category').css('display', 'grid');
@@ -20,13 +23,13 @@ $('.btn-menu').mouseover(function() {
     }
 });
 $('.header-content').mouseleave(function() {
-  // if($('header').hasClass('.down') == true) {
-  $('.logo img').attr('src', '/static/images/logo.png');
-  $('header').removeClass('white');
-  $('.btn-menu').removeClass('color');
-  $('.btn-menu').removeClass('active');
-  // }
-  $('.btn-menu').removeClass('active');
-  $('.category-box').css('display', 'none');
-  $('.header-content').css('display', 'none');
+    // if($('header').hasClass('.down') == true) {
+    $('.logo img').attr('src', '/static/images/logo.png');
+    $('header').removeClass('white');
+    $('.btn-menu').removeClass('color');
+    $('.btn-menu').removeClass('active');
+    // }
+    $('.btn-menu').removeClass('active');
+    $('.category-box').css('display', 'none');
+    $('.header-content').css('display', 'none');
 });
