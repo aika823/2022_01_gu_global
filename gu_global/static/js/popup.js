d603ccad2e6c05b3273d46fac5b0ab5a1db095ce
Starters <chrismin1205@gmail.com>
2022-06-23 17:37:06 +0900

    [front] 팝업 수정

--- gu_global/static/js/popup.js
+++ gu_global/static/js/popup.js
@@ -1,87 +1,94 @@
 $(function() {
-    // window.open('popup.html', 'windowPop', 'width=400, height=600, left=100, top=200, resizable = yes')
+  // window.open('popup.html', 'windowPop', 'width=400, height=600, left=100, top=200, resizable = yes')
 
-    // 쿠키 가져오기
-    var getCookie = function(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1);
-            if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
-        }
-        return "";
-    }
+  // 쿠키 가져오기
+  var getCookie = function(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') c = c.substring(1);
+          if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+      }
+      return "";
+  }
 
-    // 24시간 기준 쿠키 설정하기  
-    var setCookie = function(cname, cvalue, exdays) {
-        var todayDate = new Date();
-        todayDate.setTime(todayDate.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + todayDate.toUTCString(); // UTC기준의 시간에 exdays인자로 받은 값에 의해서 cookie가 설정 됩니다.
-        document.cookie = cname + "=" + cvalue + "; " + expires;
-    }
+  // 24시간 기준 쿠키 설정하기  
+  var setCookie = function(cname, cvalue, exdays) {
+      var todayDate = new Date();
+      todayDate.setTime(todayDate.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + todayDate.toUTCString(); // UTC기준의 시간에 exdays인자로 받은 값에 의해서 cookie가 설정 됩니다.
+      document.cookie = cname + "=" + cvalue + "; " + expires;
+  }
 
-    var couponClose = function() {
-        if ($("input[name='chkbox']").is(":checked") == true) {
-            setCookie("close", "Y", 1); //기간( ex. 1은 하루, 7은 일주일)
-        }
-        $(".pop").hide();
-    }
+  var couponClose = function() {
+      if ($("input[name='chkbox']").is(":checked") == true) {
+          setCookie("close", "Y", 1); //기간( ex. 1은 하루, 7은 일주일)
+      }
+      var box_length = $('.layerBox').length;
+      console.log(box_length);
 
-    function count() {
-        $('.number').each(function() {
-            $(this)
-                .prop('Counter', 0)
-                .animate({
-                    Counter: $(this).text(),
-                }, {
-                    duration: 4000,
-                    easing: 'linear',
-                    step: function(now) {
-                        $(this).text(Math.ceil(now));
-                    },
-                });
-        });
-    }
+      if(box_length != 0) {
+        $('.layerBox').last().detach()
+      }
+      $(this).parent('.layerBox').hide();
+      // $(".pop").hide();
+  }
 
-    $(document).ready(function() {
-        var cookiedata = document.cookie;
-        if (cookiedata.indexOf("close=Y") < 0) {
-            $(".pop").show();
-        } else {
-            $(".pop").hide();
-        }
-        $(".btnClose").click(function() {
-            couponClose();
-        });
-    });
+  function count() {
+      $('.number').each(function() {
+          $(this)
+              .prop('Counter', 0)
+              .animate({
+                  Counter: $(this).text(),
+              }, {
+                  duration: 4000,
+                  easing: 'linear',
+                  step: function(now) {
+                      $(this).text(Math.ceil(now));
+                  },
+              });
+      });
+  }
 
-    var count_num = 0;
+  $(document).ready(function() {
+      var cookiedata = document.cookie;
+      if (cookiedata.indexOf("close=Y") < 0) {
+          $(".pop").show();
+      } else {
+          $(".pop").hide();
+      }
+      $(".btnClose").click(function() {
+          couponClose();
+      });
+  });
 
-    var $wrap = $('.text-wrap');
-    var $page = $('.page-start');
-    var $window = $(window);
-    var pageOffsetTop = $page.offset().top;
-    var pageOffsetCount = $('.page-count').offset().top;
+  var count_num = 0;
 
-    $window.resize(function() {
-        pageOffsetTop = $page.offset().top;
-        pageOffsetCount = $('.page-count').offset().top;
-    });
+  var $wrap = $('.text-wrap');
+  var $page = $('.page-start');
+  var $window = $(window);
+  var pageOffsetTop = $page.offset().top;
+  var pageOffsetCount = $('.page-count').offset().top;
 
-    $window.on('scroll', function() {
-        var scrolled = $window.scrollTop() >= pageOffsetTop;
-        $wrap.toggleClass('down', scrolled);
+  $window.resize(function() {
+      pageOffsetTop = $page.offset().top;
+      pageOffsetCount = $('.page-count').offset().top;
+  });
 
-        var scrolled = $window.scrollTop() >= pageOffsetCount;
-        if (scrolled) {
-            if (count_num == 0) {
-                count();
-            }
-            count_num = count_num + 1;
-        }
+  $window.on('scroll', function() {
+      var scrolled = $window.scrollTop() >= pageOffsetTop;
+      $wrap.toggleClass('down', scrolled);
 
-    });
+      var scrolled = $window.scrollTop() >= pageOffsetCount;
+      if (scrolled) {
+          if (count_num == 0) {
+              count();
+          }
+          count_num = count_num + 1;
+      }
+
+  });
 
 
 });
